22e7bd593afbf85570e40c69c3a05463888e2605
randon.hyman223 <randon.hyman223@NEITD-123511>
2018-01-29 21:40:19 -0600

    CHANGE 1/29/18

--- Classwork/Section 1/Nile.Host/Program.cs
+++ Classwork/Section 1/Nile.Host/Program.cs
@@ -6,8 +6,109 @@ namespace Nile.Host
     {
         static void Main( string[] args )
         {
-            
+            bool quit = false;
+            while (!quit)
+            {
+                //Display menu
+                char choice = DisplayMenu();
+
+                //Process menu selection
+                switch (choice)
+                {
+                    case 'l':
+                    case 'L': ListProducts(); break;
+
+                    case 'a':
+                    case 'A': AddProduct(); break;
+
+                    case 'q':
+                    case 'Q': quit = true; break;
+                };
+            };
         }
+
+        static void AddProduct()
+        {   
+            //Get Name
+            _name = ReadString("Enter name : ", true);
+
+            //Get Price
+            _price = ReadDecimal("Enter price: ", 0);
+
+            //Get Description
+            _description = ReadString("Enter (if wanted) description: ", false);
+        }
+
+        private static decimal ReadDecimal( string message, decimal minValue) 
+        {
+            do
+            {
+                Console.Write(message);
+
+                string value = Console.ReadLine();
+
+                if (Decimal.TryParse(value, out decimal result))
+                { 
+
+                //If not required or not empty
+                if (result >= minValue)
+                    return result;
+            };
+                Console.WriteLine("Value must be >= {0}", minValue);
+            } while (true);
+        }
+        private static string ReadString( string message, bool isRequired )
+        {
+            do
+            {
+                Console.Write(message);
+
+                string value = Console.ReadLine();
+
+                if (isRequired || value != "");
+                return value;
+            } while (true);
+        }
+
+        private static char DisplayMenu()
+        {
+            do
+            { 
+
+                Console.WriteLine("L)ist Products");
+                Console.WriteLine("A)dd Products");
+                Console.WriteLine("Q)uit");
+
+                string input = Console.ReadLine();
+
+                if (input == "L" || input == "l")
+                    return input[0];
+                else if (input == "A")
+                    return input[0];
+                else if (input == "Q")
+                    return input[0];
+            
+                Console.WriteLine("Please choose a valid option");
+            }while (true);
+        }
+
+        static void ListProducts()
+        {
+            //Are there any products?
+            if (_name != null && _name != "")
+            {
+                //Display a product
+                Console.WriteLine(_name);
+                Console.WriteLine(_price);
+                Console.WriteLine(_description);
+            } else
+                Console.WriteLine("No products");
+        }
+            
+        //Data for a product
+        static string _name;
+        static decimal _price;
+        static string _description;
 
         static void PlayingWithPriimitives ()
         {
